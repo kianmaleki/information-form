b1f9d1df3b6950e7e122605e2111187703ba9ca8
kian maleki <150921250+kianmaleki@users.noreply.github.com>
2024-07-07 18:18:55 +0330

    "Updated HTML, JS, and CSS files: modified form structure, added new fields and options, and changed styles and layouts."

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Initialize Persian date picker
-  $("#company_verification_date").persianDatepicker({
-    altField: "#company_verification_date",
-    altFormat: "YYYY/MM/DD",
-    observer: true,
-    format: "YYYY/MM/DD",
-    initialValue: true,
-    timePicker: {
-      enabled: false,
-    },
-  });
-});
+function toggleGatewayOptions(type) {
+  var directOptions = document.getElementById("direct_gateway_options");
+  var intermediaryOptions = document.getElementById(
+    "intermediary_gateway_options"
+  );
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("input[type='file']").forEach((input) => {
-    input.addEventListener("change", function () {
-      var fileName = this.files[0] ? this.files[0].name : "";
-      var siblingElement = this.nextElementSibling;
-      if (siblingElement) {
-        siblingElement.textContent = fileName
-          ? fileName
-          : "فایلی انتخاب نشده است";
-      }
-    });
+  if (type === "direct") {
+    directOptions.style.display = "block";
+    intermediaryOptions.style.display = "none";
+    document.getElementById("intermediary_gateway_name").required = false;
+    document.getElementById("direct_gateway_name").required = true;
+    document.getElementById("direct_gateway_username").required = true;
+    document.getElementById("direct_gateway_password").required = true;
+  } else if (type === "intermediary") {
+    directOptions.style.display = "none";
+    intermediaryOptions.style.display = "block";
+    document.getElementById("direct_gateway_name").required = false;
+    document.getElementById("direct_gateway_username").required = false;
+    document.getElementById("direct_gateway_password").required = false;
+    document.getElementById("intermediary_gateway_name").required = true;
+  }
+}
+
+// Initialize Persian date picker
+document.querySelectorAll("input[type='file']").forEach((input) => {
+  input.addEventListener("change", function () {
+    var fileName = this.files[0] ? this.files[0].name : "";
+    var siblingElement = this.nextElementSibling;
+    if (siblingElement) {
+      siblingElement.textContent = fileName
+        ? fileName
+        : "فایلی انتخاب نشده است";
+    }
   });
 });
 
